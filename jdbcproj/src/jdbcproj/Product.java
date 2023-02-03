502a2a6193b4fd82fdae26b01938dcc90cc3af6e
Ravi <Ravi@DESKTOP-M7LCR10.ht.home>
2023-02-03 14:26:23 -0500

    get set

---
+++ jdbcproj/src/jdbcproj/Product.java
@@ -0,0 +1,16 @@
+package jdbcproj;
+
+public class Product {
+	private int id;
+	private String name;
+	private float price;
+	private String description;
+	private int quantity;
+	
+	public Product() 
+	{
+		//TODO auto generated constructor stub
+	}
+
+}
+
